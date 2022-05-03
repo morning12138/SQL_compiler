12b0681fcfd48aa676f198930550be790e14f6b3
ss <1820216400@qq.com>
2022-05-03 10:32:05 +0800

    LL(1) LR(1)

---
+++ syntax-analysis/LL.py
@@ -0,0 +1,307 @@
+from ctypes import sizeof
+import os
+from tabulate import tabulate
+
+VN = [] # 非终结符集
+VT = [] # 终结符集
+V = [] # 符号集 
+
+rules = [] # 文法规则
+
+first = [] # first集
+follow = [] # follow集
+
+table = []
+
+test_str = [] # 输入串 
+state_stack = [] # 状态栈
+v_stack = [] # 符号栈
+
+# 读取sql语法文件
+def read_sql_syntax():
+    global rules
+    file_path = "./sql_syntax.txt"
+    with open(file_path, encoding='utf-8') as file_obj:
+        rules = file_obj.readlines()
+    file_obj.close()
+
+    # 对lines进行处理
+    for i in range(len(rules)):
+        rules[i] = rules[i].split()
+        rules[i].pop(0)
+
+
+
+# 划分终结符和非终结符
+def get_V():
+    for rule in rules:
+        if rule[0] not in VN:
+            VN.append(rule[0])
+            V.append(rule[0])
+    for rule in rules:
+        for ru in rule[2:]:
+            if ru not in VT and ru not in VN:
+                VT.append(ru)
+                V.append(ru)
+    VT.append('#')
+    V.append('#')
+
+
+
+def init_first():
+    for vn_index in range(len(VN)):
+        first_list = []
+        first.append(first_list)
+
+def get_first():
+    
+    init_first()
+    while(True):
+        flag = 0
+        for rule in rules:
+            vn_index = get_v_index(rule[0], 2)
+            
+            for j in range(2, len(rule)):
+                if rule[j] in VT and rule[j] not in first[vn_index]:
+                    first[vn_index].append(rule[j])
+                    flag = 1
+                    break
+                if rule[j] in VN:
+                    vn2_index = get_v_index(rule[j], 2)
+                    if first[vn2_index] != []:
+                        for first_index in range(len(first[vn2_index])):
+                            if first[vn2_index][first_index] not in first[vn_index] and first[vn2_index][first_index] != '$':
+                                first[vn_index].append(first[vn2_index][first_index])
+                                flag = 1
+                
+                if in_empty_rule(rule[j]) == -1:
+                    break
+                elif j == len(rule) - 1:
+                    if '$' not in first[vn_index]:
+                        first[vn_index].append('$')
+
+
+        if flag == 0:
+            break
+
+def in_empty_rule(this_vn):
+    for rule in rules:
+        if rule[0] == this_vn and rule[2] == '$':
+            return 0
+    return -1
+
+
+def init_follow():
+    for vn_index in range(len(VN)):
+        follow_list = []
+        follow.append(follow_list)
+    root_index = get_v_index('root', 2)
+    follow[root_index].append('#')
+
+def get_follow():
+    
+    init_follow()
+    while(True):
+        flag = 0
+        for rule in rules:
+            for i in range(2, len(rule) - 1):
+                vn_index = get_v_index(rule[i], 2)
+                for j in range(i + 1, len(rule)):
+                    if rule[i] in VN and rule[j] in VT and rule[j] not in follow[vn_index]:
+                        follow[vn_index].append(rule[j])
+                        flag = 1
+                        break
+
+                    elif rule[i] in VN and rule[j] in VN:
+                        next_vn_index = get_v_index(rule[j], 2)
+                        for k in range(len(first[next_vn_index])):
+                            if first[next_vn_index][k] not in follow[vn_index]:
+                                follow[vn_index].append(first[next_vn_index][k])
+                                flag = 1
+                    
+                    if in_empty_rule(rule[j]) == -1:
+                        break 
+            
+            for i in range(1, len(rule) - 1):
+                
+                if rule[len(rule) - i] in VN:
+                    first_vn_index = get_v_index(rule[0], 2)
+                    last_vn_index = get_v_index(rule[len(rule) - i], 2)
+                    for j in range(len(follow[first_vn_index])):
+                            if follow[first_vn_index][j] not in follow[last_vn_index]:
+                                follow[last_vn_index].append(follow[first_vn_index][j])
+                                flag = 1
+                   
+                                
+                if in_empty_rule(rule[len(rule) - i]) == -1:
+                    break
+
+        if flag == 0:
+            break
+
+
+def init_table():
+
+    for vn_index in range(len(VN)):
+        tab_list = []
+        for vt_index in range(len(VT)):
+            tab_list.append(" ") 
+        table.append(tab_list)
+
+def get_table():
+
+    init_table()
+    for vn_index in range(len(VN)):
+        for vt_index in range(len(VT)):
+            for rule in rules:
+                first_vn_index = get_v_index(rule[0], 2)
+                if vn_index == first_vn_index:
+                    if rule[2] in VT:
+                        second_vt_index = get_v_index(rule[2], 1)
+                        if second_vt_index  == vt_index:
+                            rule_index = get_rule_index(rule)
+                            table[vn_index][vt_index] = rule_index
+                            
+                    elif rule[2] in VN:
+                        second_vn_index = get_v_index(rule[2], 2)
+                        if VT[vt_index] in first[first_vn_index] and VT[vt_index] in first[second_vn_index]:
+                            rule_index = get_rule_index(rule)
+                            table[vn_index][vt_index] = rule_index
+                            
+    
+        if '$' in first[vn_index] and follow[vn_index] != []:
+            for i in range(len(follow[vn_index])):
+                tmp_rule = [VN[vn_index], '->', '$']
+                rule_index = get_rule_index(tmp_rule)
+                    
+                if rule_index != -1:
+                    follow_vt_index = get_v_index(follow[vn_index][i], 1)
+                    table[vn_index][follow_vt_index] = rule_index         
+                    
+    
+def print_table(max_vn_index, max_vt_index):
+    print('\n')
+    print('----------------------------------------------TABLE---------------------------------------------')
+    
+    table_header = []
+    table_header.append('VN')
+    for vt_index in range(10):
+        table_header.append(VT[vt_index])
+
+    part_of_action = []
+    for vn_index in range(max_vn_index):
+        tmp = []
+        tmp.append(VN[vn_index])
+        for vt_index in range(max_vt_index):
+            tmp.append(table[vn_index][vt_index])
+        
+        part_of_action.append(tmp)
+
+    print(tabulate(part_of_action, headers=table_header, tablefmt='grid'))
+ 
+
+def print_first():
+    print('\n----------------------first------------------------------')
+    for i in range(len(VN)):
+        print(VN[i], first[i])
+
+
+def print_follow():
+    print('\n----------------------follow------------------------------')
+    for i in range(len(VN)):
+        print(VN[i], follow[i])
+
+
+def get_v_index(this_v, FLAG): 
+    if FLAG == 0:
+        index = 0
+        for v in V:
+            if(this_v == v):
+                return index
+            index = index + 1
+        return -1
+    elif FLAG == 1:
+        index = 0
+        for vt in VT:
+            if this_v == vt:
+                return index
+            index = index + 1
+        return -1
+    elif FLAG == 2:
+        index = 0
+        for vn in VN:
+            if this_v == vn:
+                return index
+            index = index + 1
+        return -1
+    else:
+        return -1
+
+def get_rule_index(this_rule):
+    index = 1
+    for rule in rules:
+        if this_rule == rule:
+            return index
+        index = index + 1
+    return -1
+
+
+
+# 对输入串进行语法分析
+if __name__ == '__main__':
+    read_sql_syntax()
+    get_V()
+    get_first()
+
+    print_first()
+    
+    get_follow()
+
+    get_table()
+
+    print_table(10,10)
+
+    # 输入形式 SELECT IDN . IDN FROM IDN WHERE IDN . IDN > INT
+    str = input()
+    str = str + ' #'
+    test_str = str.split(' ')
+
+    state_stack.append(0)
+    v_stack.append('#')
+    v_stack.append('root')
+
+    step = 1
+    while test_str != None:
+        a = test_str[0][:]
+
+        if a in VT:
+            if a == v_stack[-1] and a != '#':
+
+                print(step, '/', v_stack[-1] + '#' + a, 'move')
+                step = step + 1
+
+                v_stack.pop()
+                test_str.pop(0)
+
+            elif a == v_stack[-1] and a == '#':
+                print(step, rule_index, v_stack[-1] + '#' + a, 'accept')
+                break
+            else:
+                rule = []
+                vt_index = get_v_index(a, 1)
+                vn_index = get_v_index(v_stack[-1], 2)
+                rule_index = table[vn_index][vt_index]
+                rule = rules[rule_index - 1][:]
+
+                print(step, rule_index, v_stack[-1] + '#' + a, 'reduction')
+                step = step + 1
+
+                v_stack.pop()
+                
+                for i in range(len(rule) - 2):
+                    tmp = rule[len(rule) - i - 1][:]
+                    if tmp != '$':
+                        v_stack.append(tmp)               
+        else:
+            print(step, rule_index, v_stack[-1] + '#' + a, 'error')
+            break
---
+++ syntax-analysis/LR.py
@@ -0,0 +1,476 @@
+# 不符合LR（0）文法（存在推出空的文法规则），符合LR(1)文法
+# sql_syntax语法终结符中有. ,在构造项目时用`代替.
+
+from ctypes import sizeof
+import os
+from re import S
+from turtle import dot, st
+from hamcrest import none
+from matplotlib.pyplot import new_figure_manager
+from tabulate import tabulate
+from torch import le
+import wcwidth
+
+class Standard_item:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+VN = [] # 非终结符集
+VT = [] # 终结符集
+V = [] # 符号集 
+
+rules = [] # 文法规则
+items = [] # 项目集
+standard_items = [] # 规范项目集
+
+first = [] # first集
+
+action = [] # action表
+goto = [] # goto表
+
+
+test_str = [] # 输入串 
+state_stack = [] # 状态栈
+v_stack = [] # 符号栈
+
+# 读取sql语法文件
+def read_sql_syntax():
+    global rules
+    file_path = "./sql_syntax.txt"
+    with open(file_path, encoding='utf-8') as file_obj:
+        rules = file_obj.readlines()
+    file_obj.close()
+
+    # 对lines进行处理
+    for i in range(len(rules)):
+        rules[i] = rules[i].split()
+        rules[i].pop(0)
+
+def init_first():
+    for vn_index in range(len(VN)):
+        first_list = []
+        first.append(first_list)
+
+def get_first():
+    
+    init_first()
+    while(True):
+        flag = 0
+        
+        for rule in rules:
+            vn_index = get_v_index(rule[0], 2)
+            for j in range(2, len(rule)):
+                if rule[j] in VT and rule[j] not in first[vn_index]:
+                    first[vn_index].append(rule[j])
+                    flag = 1
+                    break
+                if rule[j] in VN:
+                    vn2_index = get_v_index(rule[j], 2)
+                    if first[vn2_index] != []:
+                        for first_index in range(len(first[vn2_index])):
+                            if first[vn2_index][first_index] not in first[vn_index] and first[vn2_index][first_index] != '$':
+                                first[vn_index].append(first[vn2_index][first_index])
+                                flag = 1
+
+                if in_empty_rule(rule[j]) == -1:
+                    break
+                elif j == len(rule) - 1:
+                    if '$' not in first[vn_index]:
+                        first[vn_index].append('$')
+
+        if flag == 0:
+            break
+
+
+def in_empty_rule(this_vn):
+    for rule in rules:
+        if rule[0] == this_vn and rule[2] == '$':
+            return 0
+    return -1
+
+
+# 构造文法项目
+def get_items():
+    items.append(['root\'', '->', '`', 'root'])
+    items.append(['root\'', '->', 'root', '`'])
+    for rule in rules:
+        for i in range(len(rule)-1):
+            if rule[-1] != '$':
+                tmp = rule[:2+i] + list('`') + rule[2+i:]
+                items.append(tmp)
+            else:
+                tmp = rule[:2] + list('`')
+                items.append(tmp)
+    
+# 划分终结符和非终结符
+def get_V():
+    for rule in rules:
+        if rule[0] not in VN:
+            VN.append(rule[0])
+            V.append(rule[0])
+    for rule in rules:
+        for ru in rule[2:]:
+            if ru not in VT and ru not in VN:
+                VT.append(ru)
+                V.append(ru)
+    VT.append('#')
+    V.append('#')
+
+
+# 闭包运算
+def get_closeure(set):
+    closure = []
+    tag = 0
+    for it in set:
+        if in_closure(it, closure) == -1:
+            closure.append(it)
+        it_left = it.left[:]
+        it_right = it.right[:]
+        dot_index = it_left.index('`')
+        if dot_index != len(it_left) - 1:
+            if it_left[dot_index + 1] in VN:
+                tmp_right = []
+                
+                if len(it_left) - dot_index == 2:
+                    tmp_right = it_right[:]
+                elif it_left[dot_index + 2] in VT:
+                    tmp_right.append(it_left[dot_index + 2])
+                elif it_left[dot_index + 2] in VN:
+                    vn_index = get_v_index(it_left[dot_index + 2], 2)
+                    tmp_right = first[vn_index][:]
+
+                for item in items:
+                    if item[0] == it_left[dot_index + 1] and item[2] == '`' :
+                        tmp = Standard_item(item, tmp_right)
+                        if in_closure(tmp, closure) == -1:                        
+                            set.append(tmp)
+                            closure.append(tmp)
+                        elif in_closure(tmp, closure) == 1:
+                            for i in range(len(closure)):
+                                if it_left == closure[i].left:
+                                    for j in range(it_right):
+                                        if it_right[j] not in closure[i].right: 
+                                            closure[i].right.append(it_right[j])
+            else:
+                continue 
+        
+        else:
+            continue
+    return closure
+
+# 构造项目集规范族standard_items
+def get_standard_items():
+    # 构造第一项
+    set = []
+    tmp = Standard_item(items[0], ['#'])
+    set.append(tmp)
+    standard_items.append(get_closeure(set))
+
+    for standard_item in standard_items:
+        for v in V:
+            new_item = get_new_item(standard_item, v)
+            if new_item != None and in_standard_items(new_item) == -1:           
+                standard_items.append(new_item)
+
+
+# 得到新的standard_item
+def get_new_item(standard_item, v):
+
+    set = []
+    for i in range(len(standard_item)):
+        it = standard_item[i].left[:]
+
+        dot_index =it.index('`')
+        if dot_index != len(it) - 1:
+            if it[dot_index + 1] == v:
+                tmp_left = it[:]
+                tmp_left[dot_index] = tmp_left[dot_index + 1]
+                tmp_left[dot_index + 1] = '`'  
+                tmp_right = standard_item[i].right[:]
+                tmp = Standard_item(tmp_left, tmp_right)
+                set.append(tmp)
+
+        if set != []:
+            return get_closeure(set)
+    return None
+
+def in_closure(it, closure):
+    for i in range(len(closure)):
+        if it.left == closure[i].left and it.right == closure[i].right:
+            return 0
+        if it.left == closure[i].left and it.right != closure[i].right:
+            return 1
+    return -1
+
+# 判断standard_item是否已存在
+def in_standard_items(new_item):
+
+    for standard_item in standard_items:
+        flag = 0
+        for i in range(len(standard_item)):
+            for j in range(len(new_item)):
+                if standard_item[i].left == new_item[j].left and standard_item[i].right == new_item[j].right:
+                    flag = flag + 1
+                    break
+        if flag == len(standard_item) and len(standard_item) == len(new_item):
+            return 0
+    return -1
+
+
+# 构造action
+def init_action():
+    for item_index in range(len(standard_items)):
+        ac_list = []
+        for vt_index in range(len(VT)):
+            ac_list.append(" ") 
+        action.append(ac_list)
+
+
+def get_action():
+    # action表初始化
+    init_action()
+    for standard_item in standard_items:
+        item_index = get_standard_item_index(standard_item)
+        for it in standard_item:
+            it_left = it.left[:]
+            it_right = it.right[:]
+            dot_index = it_left.index('`')
+            if it_left[dot_index + 1:] == []:
+                # 规约
+                rule_index = get_rule_index(it_left)
+                
+                if '$' in it_right:
+                    for vt_index in range(len(VT)):
+                        if action[item_index][vt_index] == ' ':
+                            action[item_index][vt_index] = 'r' + str(rule_index)
+                    
+                else:
+                    for vt_index in range(len(VT)):
+                        if VT[vt_index] in it_right:
+                            action[item_index][vt_index] = 'r' + str(rule_index)
+                       
+            else:
+                for vt in VT: 
+                    vt_index = get_v_index(vt, 1)
+                    if vt == it_left[dot_index + 1]:
+                        next_item_index = get_standard_item_index(get_new_item(standard_item, vt))
+                        action[item_index][vt_index] = 's' + str(next_item_index)
+                        
+    action[1][len(VT)-1] = 'acc'
+
+def init_goto():
+    for item_index in range(len(standard_items)):
+        goto_list = []
+        for vn_index in range(len(VN)): 
+            goto_list.append(" ") 
+        goto.append(goto_list)
+
+
+def get_goto():
+    # 初始化goto表
+    init_goto()
+
+    for standard_item in standard_items:
+        item_index = get_standard_item_index(standard_item)
+        for vn in VN:
+            vn_index = get_v_index(vn, 2)
+            next_item = get_new_item(standard_item, vn)
+            if next_item != None and in_standard_items(next_item) != -1:
+                next_item_index = get_standard_item_index(next_item)
+                goto[item_index][vn_index] = next_item_index
+
+def get_standard_item_index(this_item):
+    index = 0
+    for standard_item in standard_items:
+        flag = 0
+        for i in range(len(standard_item)):
+            for j in range(len(this_item)):
+                if standard_item[i].left == this_item[j].left and standard_item[i].right == this_item[j].right:
+                    flag = flag + 1
+                    break
+        if flag == len(standard_item) and len(standard_item) == len(this_item):
+            return index
+        index = index + 1
+    return -1
+
+def get_v_index(this_v, FLAG): 
+    if FLAG == 0:
+        index = 0
+        for v in V:
+            if(this_v == v):
+                return index
+            index = index + 1
+        return -1
+    elif FLAG == 1:
+        index = 0
+        for vt in VT:
+            if this_v == vt:
+                return index
+            index = index + 1
+        return -1
+    elif FLAG == 2:
+        index = 0
+        for vn in VN:
+            if this_v == vn:
+                return index
+            index = index + 1
+        return -1
+    else:
+        return -1
+
+def get_rule_index(this_item):
+    index = 1
+    tmp = this_item[:]
+    for rule in rules:
+        if this_item == rule + list('`'):
+            return index
+        elif len(this_item) == 3 and this_item[2] == '`':
+            if index == 1:
+                tmp.insert(2, '$')
+            if tmp == rule + list('`'):
+                return index
+        index = index + 1
+    if this_item == items[1]:
+        return 0
+    return -1
+
+def print_action():
+    print('\n')
+    print('----------------------------------------------ACTION---------------------------------------------')
+    
+    table_header = []
+    table_header.append('state')
+    for vt_index in range(10):
+        table_header.append(VT[vt_index])
+
+    part_of_action = []
+    for item_index in range(10):
+        tmp = []
+        for vt_index in range(10):
+            tmp.append(action[item_index][vt_index])
+        part_of_action.append(list(str(item_index)) + tmp)
+
+    print(tabulate(part_of_action, headers=table_header, tablefmt='grid'))
+
+
+def print_goto():
+    print('\n')
+    print('-----------------------------------------------GOTO---------------------------------------------')
+    table_header = []
+    table_header.append('state')
+    for vn_index in range(10):
+        table_header.append(VN[vn_index])
+
+    part_of_goto = []
+    for item_index in range(10):
+        tmp = []
+        for vn_index in range(8):
+            tmp.append(goto[item_index][vn_index])
+        part_of_goto.append(list(str(item_index)) + tmp)
+
+    print(tabulate(part_of_goto, headers=table_header, tablefmt='grid'))
+
+# 对输入串进行语法分析
+if __name__ == '__main__':
+    read_sql_syntax()
+    
+    get_items()
+    get_V()
+
+    get_first()
+    # print('first', first)
+
+    get_standard_items()
+
+    get_action()
+    # print_action()
+    get_goto()
+    # print_goto()
+
+    print('0')
+    for i in range(len(standard_items[0])):
+        print(standard_items[0][i].left)
+        print(standard_items[0][i].right)
+
+    print('9')
+    for i in range(len(standard_items[9])):
+        print(standard_items[9][i].left)
+        print(standard_items[9][i].right)
+
+    print('21')
+    for i in range(len(standard_items[21])):
+        print(standard_items[21][i].left)
+        print(standard_items[21][i].right)
+
+    print('54')
+    for i in range(len(standard_items[54])):
+        print(standard_items[54][i].left)
+        print(standard_items[54][i].right)
+
+    print('48')
+    for i in range(len(standard_items[48])):
+        print(standard_items[48][i].left)
+        print(standard_items[48][i].right)
+
+    print('79')
+    for i in range(len(standard_items[79])):
+        print(standard_items[79][i].left)
+        print(standard_items[79][i].right)
+
+    # 输入形式 SELECT IDN . IDN FROM IDN WHERE IDN . IDN > INT
+    str = input()
+    str = str + ' #'
+    test_str = str.split(' ')
+
+    state_stack.append(0)
+    v_stack.append('#')
+
+    step = 1
+    while test_str != None:
+        a = test_str[0]
+
+        if a in VT:
+            a_index = get_v_index(a, 1)
+            print('a', a)
+            act = action[state_stack[-1]][a_index]
+            print(act)
+            # 移进
+            if act[0] == 's':
+                test_str.pop(0)
+                state_stack.append(int(act[1:]))
+                print(step, '/', v_stack[-1] + '#' + a, 'move')
+                v_stack.append(a)
+                step = step + 1
+            # 规约
+            elif act[0] == 'r': 
+                
+                if rules[int(act[1:]) - 1][-1] != '$' or len(rules[int(act[1:]) - 1]) != 3:
+                    for j in range(len(rules[int(act[1:]) - 1]) - 2):
+                        state_stack.pop()
+                    
+                v_stack.pop()
+                next_v = rules[int(act[1:]) - 1][0]
+                v_stack.append(next_v)              
+                v_index = get_v_index(v_stack[-1], 2)       
+                next_state = goto[state_stack[-1]][v_index]
+                state_stack.append(next_state)
+            
+
+                print(step, int(act[1:]), v_stack[-1] + '#' + a, 'reduction\n')
+                step = step + 1
+            # 接受
+            elif act == 'acc':
+                print(step, '1', v_stack[-1] + '#' + a, 'accept')
+                break
+            else:
+                print(step, '/' , v_stack[-1] + '#' + a, 'error')
+                break
+        else:
+            print(step, '/' , v_stack[-1] + '#' + a, 'error')
+            break
+
+
+
+
